1254a67404152eaca7b8f34bdf0e021c03a6b38f
Elijah Kelting <elijahakbusiness@gmail.com>
2024-10-07 13:01:08 -0600

    movement system

--- Assets/Scripts/Input/InputComponent.cs
+++
@@ -1,9 +0,0 @@
-using Unity.Entities;
-using Unity.Mathematics;
-
-public struct InputComponent : IComponentData
-{
-    public float2 mousePosition;
-    public float2 movement;
-    public bool pressLMB;
-}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 10f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = moveInput * moveSpeed * Time.deltaTime;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        animator.SetBool("isWalking", true);
+
+        if(context.canceled)
+        {
+            animator.SetBool("isWalking", false);
+            animator.SetFloat("LastX", moveInput.x);
+            animator.SetFloat("LastY", moveInput.y);
+        }
+        moveInput = context.ReadValue<Vector2>();
+        animator.SetFloat("CurX", moveInput.x);
+        animator.SetFloat("CurY", moveInput.y);
+    }
+}
