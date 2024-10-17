620eabd96c21c59519a49f9c6d87d840f5031e29
Elijah Kelting <elijahakbusiness@gmail.com>
2024-10-17 15:44:23 -0600

    NavMesh Implemented

--- Assets/Scripts/Enemies/Basic AI.cs
+++ Assets/Scripts/Enemies/Basic AI.cs
@@ -1,28 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
+using static UnityEngine.Rendering.HableCurve;
 
 public class BasicAI : MonoBehaviour
 {
-    private Rigidbody2D rb;
+    [SerializeField] Transform target;
+    [SerializeField] int segments;
+    private int distance = 1;
+    private NavMeshAgent agent;
     private Animator animator;
-    private RaycastHit2D hit;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        agent = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        int startAngle = 0;
+        int finishAngle = 360;
+        int increment = (int)(finishAngle / segments);
+        Vector2 targetPos = Vector2.zero;
+        Vector2 endPos;
+
+        for (int i = startAngle; i < finishAngle; i += increment)
+        {
+            Vector2 rayDirection2D = (Quaternion.Euler(0, 0, i) * transform.up).normalized;
+            targetPos = rayDirection2D * distance;
+            endPos = new Vector2(transform.position.x + targetPos.x, transform.position.y + targetPos.y);
+
+            RaycastHit2D hit2D = Physics2D.Raycast(transform.position, rayDirection2D, distance);
+            if (hit2D.collider != null)
+            {
+                Debug.DrawLine(transform.position, endPos, Color.green);
+                Move();
+            }
+            else
+            {
+                Debug.DrawLine(transform.position, endPos, Color.red);
+            }
+        }
+        if (agent.remainingDistance <= 0.5)
+        {
+            animator.SetBool("isWalking", false);
+        }
+        else
+        {
+            animator.SetFloat("LastX", agent.velocity.x);
+            animator.SetFloat("LastY", agent.velocity.y);
+            animator.SetFloat("CurX", agent.velocity.x);
+            animator.SetFloat("CurY", agent.velocity.y);
+        }
     }
 
-    private void FixedUpdate()
+    private void Move()
     {
-        
+        animator.SetBool("isWalking", true);
+        agent.SetDestination(target.position);
+    }
+
+    private void Wander()
+    {
+        animator.SetBool("isWalking", true);
     }
 }
