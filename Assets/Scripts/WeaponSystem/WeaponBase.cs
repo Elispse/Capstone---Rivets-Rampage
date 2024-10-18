62d03e5979cd26e7c106140ced37858136e73a6b
Elijah Kelting <elijahakbusiness@gmail.com>
2024-10-18 14:59:10 -0600

    WeaponBaseSetup

--- Assets/Scripts/WeaponSystem/WeaponBase.cs
+++ Assets/Scripts/WeaponSystem/WeaponBase.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WeaponBase : MonoBehaviour
+public abstract class WeaponBase : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] protected float damage = 1;
+    [SerializeField] protected float attackRate = 1;
+    [SerializeField] protected string tagName;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public abstract bool Use();
+    public abstract void Attack();
 }
