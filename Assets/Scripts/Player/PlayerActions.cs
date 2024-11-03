1316c3effb154b99b11c88a590ae717d6484db80
Elijah Kelting <elijahakbusiness@gmail.com>
2024-11-02 18:00:08 -0600

    adding assets and rooms. Started on player movement

--- Assets/Scripts/Player/PlayerActions.cs
+++ Assets/Scripts/Player/PlayerActions.cs
@@ -5,18 +5,41 @@ using UnityEngine.InputSystem;
 
 public class PlayerActions : MonoBehaviour, IDamagable
 {
+    [SerializeField] float Health;
     [SerializeField] WeaponBase weapon;
+    [SerializeField] WeaponBase[] weapons;
     [SerializeField] protected Animator animator;
+    private int currentWeapon = 0;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        Health = 6;
+
+    }
 
     public void ApplyDamage(float damage)
     {
-
+        Health -= damage;
     }
 
     public void Fire(InputAction.CallbackContext context)
     {
-        //weapon.Use(animator);
+        weapon.Use(animator);
         weapon.Attack();
         //animator.SetBool("hasAttacked", true);
+    }
+
+    public void SwapWeapons(InputAction.CallbackContext context)
+    {
+        if (currentWeapon == 0)
+        {
+            currentWeapon = 1;
+        }
+        else
+        {
+            currentWeapon = 0;
+        }
+        weapon = weapons[currentWeapon];
     }
 }
